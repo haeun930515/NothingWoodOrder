3e13bf4a03a5afa47aaaa08ccaa4521f37f5596c
Haeun <haeun930515@gmail.com>
2023-04-09 01:16:53 +0900

    4월9일 일요일 피드백 이전 기능 완성

--- src/App.js
+++ src/App.js
@@ -10,31 +10,21 @@ import './App.css';
 
 export default function App() {
 
-  const canvasRef = useRef(null);
-
-  const onHtmlToPng = () => {
-    const onCapture = () => {html2canvas(document.getElementById('capture')).then(canvas => {
-      onSaveAs(canvas.toDataURL("image/png"), "image-download.png")
-    });
-  };
-    const onSaveAs = (uri, filename) => {
-      var link = document.createElement('a');
-      document.body.appendChild(link);
-      link.href = uri;
-      link.download = filename;
+  function captureScreenshot() {
+    html2canvas(document.querySelector("#capture")).then(function(canvas) {
+      const link = document.createElement("a");
+      link.download = "screenshot.png";
+      link.href = canvas.toDataURL("image/png");
       link.click();
-      document.body.removeChild(link);
-    }
+    });
   }
 
-
-
-  //가로 길이 화살표, 세로 길이 화살표 state
+  //가로 길이 화살표, 세로 길이 화살표, 박스 개수 list state
   const [verticalList,setVerticalList] = useState([]);
   const [horizontalList, setHorizontalList] = useState([]);
+  const [boxList, setBoxList] = useState([]);
 
-
-  const addVer = () => {
+  const addHor = () => {
     setVerticalList([...verticalList, 
     <Rnd enableResizing={{ 
       top:false, right:true, 
@@ -45,12 +35,12 @@ export default function App() {
       style={{display:"flex",flexDirection:"column"}}>
         <div style={{height:"3px",width:"100%",backgroundColor:"black",marginBottom:"5px",border:"black 1px solid"}}/>
         <div style={{display:"flex",justifyContent:"center"}}>
-          <input placeholder="거리(mm)" style={{height:"20px",width:"80px",fontSize:"10px"}}/>
+          <input placeholder="거리(mm)" style={{height:"40px",width:"100px",fontSize:"17px"}}/>
         </div>
     </Rnd>]);
   };
 
-  const addHor = () => {
+  const addVer = () => {
     setHorizontalList([...horizontalList, 
       <Rnd default={{x: 0,y: 0,width: 40,height: 200}}
       enableResizing={{ 
@@ -60,15 +50,33 @@ export default function App() {
       bottomLeft:false, topLeft:false }} style={{display:"flex"}}>
       <div style={{height:"100%",width:"3px",backgroundColor:"black",marginRight:"5px",border:"black 1px solid"}}/>
       <div style={{display:"flex",alignItems:"center"}}>
-      <input placeholder="거리(mm)" style={{height:"20px",width:"80px",fontSize:"10px"}}/>
+      <input placeholder="거리(mm)" style={{height:"40px",width:"100px",fontSize:"17px"}}/>
       </div>
     </Rnd>]);
   };
   
+  const addBox = () => {
+    setBoxList([...boxList,
+      <Rnd className="TB-slot-wrapper"
+      default={{x: 0,y: 0,width: 200,height: 200}}
+      enableResizing={{
+        top:false, right: false,
+        bottom:false, left: false,
+        topRight:false, bottomRight:true,
+        bottomLeft:false, topLeft:false
+      }}>
+      <div className="TB-slot"> 
+              박스 가로, 세로
+      <input placeholder="가로(mm)" className="input-box"/>
+      <input placeholder="세로(mm)" className="input-box"/>
+    </div>
+  </Rnd>]);
+    
+  }
+
   //전체 보드 가로, 세로, 박스 개수 입력값
   const [width, setWidth] = useState('');
   const [height, setHeight] = useState('');
-  const [boxNum, setBoxNum] = useState('');
 
   const handleWidth = (e) => {
     setWidth(e.target.value);
@@ -76,68 +84,31 @@ export default function App() {
   const handleHeight = (e) => {
     setHeight(e.target.value);
   }
-  const handleBoxNum = (e) => {
-    setBoxNum(e.target.value);
-  }
 
 
   //전체 보드 가로, 세로 입력, 생성 클릭후 --> 결정값
   const [buildWidth, setBuildWidth] = useState('');
   const [buildHeight, setBuildHeight] = useState('');
-  const [buildBoxNum, setBuildBoxNum] = useState('');
 
 
 
-  //전체 보드 가로, 세로, 박스개수 입력 함수
+  //전체 보드 가로, 세로입력 함수
   const {register, handleSubmit} =useForm();
   const onSubmit = () => {
     setBuildWidth(width);
     setBuildHeight(height);
-    setBuildBoxNum(boxNum);
   }
 
   const divStyle = {
     width: buildWidth/3+"px",
     height: buildHeight/3+"px",
     border: "black 1px solid",
-    padding: "15px"
   }
-
-  //박스 input 컴포넌트 생성 함수
-  const boxInputs = (boxNum) => {
-    const boxs = [];
-    for(let i = 0; i< boxNum;i++){
-      boxs.push(
-        <Rnd key={i} className="TB-slot-wrapper"
-        default={{x: 0,y: 0,width: 200,height: 200}}>
-            <div className="TB-slot"> 
-                박스 가로, 세로
-                <input placeholder="가로(mm)" className="input-box"/>
-                <input placeholder="세로(mm)" className="input-box"/>
-            </div>
-        </Rnd>
-      );
-    }
-    return boxs;
-  }
-
-  
-
-  const handleCapture = () => {
-    const canvas = canvasRef.current
-    const image = canvas.toDataURL('image/png');
-    const link = document.createElement('a');
-    link.download = 'capture.png';
-    link.href = image;
-    link.click();
-  }
-
-
 
 
   return (
     //header
-    <Wrapper id="capture">
+    <Wrapper>
       <div className="App-Header">
         <img className="App-Header-Logo" src={NothingLogo}/>
         <img className="App-Header-Logo" src={NothingLogoText}/>
@@ -146,8 +117,9 @@ export default function App() {
         <div className="Title-Text">노씽공방 측정기 테스트</div>
         <div className="Title-Divider"></div>
       </div>
-    <div style={{display:"flex"}}>
+    <div style={{display:"flex", padding:"15px"}}>
     <div className="Input-Form">
+    <div style={{fontSize:"20px",backgroundColor:"white",padding:"2px",marginBottom:"15px"}}> 도안 생성</div>
       <form onSubmit={handleSubmit(onSubmit)}>
           <div className="input-col">
             <div>전체 가로 길이(mm)</div>
@@ -157,33 +129,35 @@ export default function App() {
             <div>전체 세로 길이(mm)</div>
             <input value={height} onChange={handleHeight}/>
           </div>
-          <div className="input-col">
-            <div>박스 개수</div>
-            <input value={boxNum} onChange={handleBoxNum}/>
-          </div>
-        <input type="submit" />
+        <input type="submit"/>
       </form>
     </div>
 
     <div className="Input-Form">
-      <div> 세로 길이 입력 추가</div>
-    <button onClick={addVer}>addVer</button>
-    <div>가로 길이 입력 추가</div>
-    <button onClick={addHor}>addHor</button>
+      <div style={{fontSize:"20px",backgroundColor:"white",padding:"2px"}}> 입력 값</div>
+      <button className="input-button" onClick={addVer}>세로 수치 추가</button>
+      <button className="input-button" onClick={addHor}>가로 수치 추가</button>
+      <button className="input-button" onClick={addBox}>박스 추가</button>
     </div>
 
     </div>
-    <div style={{padding:"15px"}} >
-        <div> 전체 가로 : {buildWidth}, 전체 세로: {buildHeight} </div>
-        <div style={divStyle}>
-          {boxInputs(buildBoxNum)}
+    <div style={{padding:"20px"}}>
+      
+      <div id="capture" style={{padding:"25px"}}>
+        <div>
+          <div> 전체 가로 : {buildWidth}, 전체 세로: {buildHeight} </div>
+          <div style={divStyle}>
+          {boxList}
           {verticalList}
           {horizontalList}
-        </div>
+          </div>
         
+        </div>
       </div>
-
-      <canvas ref={canvasRef} />
+      
+      <button className="input-button" onClick={captureScreenshot}>도안 캡쳐</button>
+      
+    </div>
     </Wrapper>
   );
 }
